a717e76bb2a190da3974699eaee933a3ea898ffb
kevinnadr <kevin19305.ib@gmail.com>
2024-05-14 18:30:08 +0700

    mengakses nim melalui static member function

--- mbrStaticfunc.cpp
+++ mbrStaticfunc.cpp
@@ -6,4 +6,42 @@ class mahasiswa
 {
 private:
     static int nim;
+public:
+    int id;
+    string nama;
 
+    void setID();
+    void printAll();
+
+    static void setNim(int pNim) { nim = pNim; }
+    static int getNim() {return nim; } 
+
+    mahasiswa(string pnama)
+    {
+        nama = pnama;
+        setID();
+    }
+};
+
+int mahasiswa::nim = 0;
+
+void mahasiswa::setID()
+{
+    id = ++nim;
+}
+
+void mahasiswa::printAll()
+{
+    cout << "ID    = " << id << endl;
+    cout << "Nama  = " << nama << endl;
+    cout << endl;
+}
+
+
+int main()
+{
+    mahasiswa mhs1("Sri Dadi");
+    mahasiswa mhs2("Budi Jatmiko");
+
+    mahasiswa::setNim(9); // mengakses nim melalui static member function
+   
